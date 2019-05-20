56e252c94200e521868f2c69c64b29b781e0a700
Murad Ali Ahmad Hassan <mura0273@edu.eal.dk>
2019-05-20 14:15:50 +0200

    Add project files.

---
+++ PapasPapbar/Application/BoardgameRepos.cs
@@ -0,0 +1,188 @@
+﻿using PapasPapbar.Application;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PapasPapbar.Application
+{
+
+    public static class BoardgameRepos
+    {
+        static string boardgameId = txtboardgameId.Text;
+        static string boardgameName;
+        static string numberOfPlayers;
+        static string audience;
+        static string expectedGameTime;
+        static string distributor;
+        static string gameTag;
+
+        public static SqlCommand cmd;
+        public static SqlDataReader reader;
+
+        public static void ResetBoardgame()
+        {
+            txtboardgameId.Text = "";
+            txtBrætspil.Text = "";
+            txtAntal.Text = "";
+            txtAldersgruppe.Text = "";
+            txtSpilletid.Text = "";
+            txtDistrubutør.Text = "";
+            txtGenre.Text = "";
+            txtBrætspil.Focus();
+            btnUpdate.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnInsert.IsEnabled = true;
+        }
+
+        public static void GetBoardgame()
+        {
+            using (SqlConnection con = new SqlConnection(DataBaseController.connectionString))
+
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd2 = new SqlCommand("ViewGameLibrary", con);                 
+                                  
+                    con.Close();
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+        }
+      
+        public static void InsertBoardgame()
+        {
+            using (SqlConnection con = new SqlConnection(DataBaseController.connectionString))
+
+                try
+                {
+                    con.Open();
+                    cmd = new SqlCommand("Insert into Game_Library (Boardgame_Name, Player_Count, Audience, Game_Time, Distributor, GameTag) Values (@Boardgame_Name, @Player_Count, @Audience, @Game_Time, @Distributor, @GameTag)", con);
+                    cmd.Parameters.AddWithValue("Boardgame_Name", boardgameId.Trim());
+                    cmd.Parameters.AddWithValue("Player_Count", txtAntal.Text.Trim());
+                    cmd.Parameters.AddWithValue("Audience", txtAldersgruppe.Text.Trim());
+                    cmd.Parameters.AddWithValue("Game_Time", txtSpilletid.Text.Trim());
+                    cmd.Parameters.AddWithValue("Distributor", txtDistrubutør.Text.Trim());
+                    cmd.Parameters.AddWithValue("GameTag", txtGenre.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+        }
+
+        public static void DeleteBoardgame()
+        {
+            using (SqlConnection con = new SqlConnection(DataBaseController.connectionString))      
+
+                try
+                {
+                    con.Open();
+                    cmd = new SqlCommand("Delete From Game_Library Where Boardgame_Id = @Boardgame_Id", con);
+                    cmd.Parameters.AddWithValue("Boardgame_Id", txtId.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    con.Close();                   
+                   // GetBoardgame();
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+        }
+
+        public static void SearchBoardgame()
+        {
+
+            using (SqlConnection con = new SqlConnection(DataBaseController.connectionString))
+
+                try
+                {
+                    con.Open();
+                    cmd = new SqlCommand("Select * From Game_Library Where Boardgame_Name Like '%" + txtSearch.Text.Trim() + "%' OR Player_Count Like '%" + txtSearch.Text.Trim() + "%' OR Game_Time Like '%" + txtSearch.Text.Trim() + "%' OR Distributor Like '%" + txtSearch.Text.Trim() + "%' OR GameTag Like '%" + txtSearch.Text.Trim() + "%' Order By Boardgame_Id Desc", con);
+                    reader = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    con.Close();
+
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+        }
+
+        public static void UpdateBoardgame()
+        {
+
+            using (SqlConnection con = new SqlConnection(DataBaseController.connectionString))
+
+                try
+                {
+                    con.Open();
+                    cmd = new SqlCommand("UPDATE Game_Library SET Boardgame_Name = @Boardgame_Name, Player_Count = @Player_Count, Audience = @Audience, Game_Time = @Game_Time, Distributor = @Distributor, GameTag = @GameTag WHERE Boardgame_Id = @Boardgame_Id", con);
+                    cmd.Parameters.AddWithValue("Boardgame_Id", txtId.Text.Trim());
+                    cmd.Parameters.AddWithValue("Boardgame_Name", txtBrætspil.Text.Trim());
+                    cmd.Parameters.AddWithValue("Player_Count", txtAntal.Text.Trim());
+                    cmd.Parameters.AddWithValue("Audience", txtAldersgruppe.Text.Trim());
+                    cmd.Parameters.AddWithValue("Game_Time", txtSpilletid.Text.Trim());
+                    cmd.Parameters.AddWithValue("Distributor", txtDistrubutør.Text.Trim());
+                    cmd.Parameters.AddWithValue("GameTag", txtGenre.Text.Trim());
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    GetBoardgame();
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+        }
+
+        public static void SelectionChangedBoardgame()
+        {
+
+            using (SqlConnection con = new SqlConnection(DataBaseController.connectionString))
+
+
+                try
+                {
+                    DataGrid dg = (DataGrid)sender;
+                    DataRowView rowSelected = dg.SelectedItem as DataRowView;
+                    if (rowSelected != null)
+                    {
+                        txtId.Text = rowSelected[0].ToString();
+                        txtBrætspil.Text = rowSelected[1].ToString();
+                        txtAntal.Text = rowSelected[2].ToString();
+                        txtAldersgruppe.Text = rowSelected[3].ToString();
+                        txtSpilletid.Text = rowSelected[4].ToString();
+                        txtDistrubutør.Text = rowSelected[5].ToString();
+                        txtGenre.Text = rowSelected[6].ToString();
+                    }
+                    btnUpdate.IsEnabled = true;
+                    btnDelete.IsEnabled = true;
+                    btnInsert.IsEnabled = false;
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+        }
+        
+    }
+}
---
+++ PapasPapbar/UI/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using PapasPapbar.Application;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PapasPapbar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private SqlCommand cmd;
+        private SqlDataReader reader;
+
+       
+
+         public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            reader = cmd.ExecuteReader();
+          
+            dt.Load(reader);
+            txtBrætspil.Focus();
+            BoardgameRepos.GetBoardgame();
+            DataGrid1.Columns[0].Visibility = Visibility.Collapsed;
+            DataGrid1.ItemsSource = dt.DefaultView;
+           
+        }
+
+        //Get Data for datagrid
+       
+
+        //Nulstil Boardgame
+        public void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            BoardgameRepos.ResetBoardgame();
+        }
+
+
+
+        //Indsætfunktion til Boardgame
+        public void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BoardgameRepos.InsertBoardgame();
+                MessageBox.Show("Record Save Successfully", "Saved", MessageBoxButton.OK);
+                BoardgameRepos.GetBoardgame();
+                BoardgameRepos.ResetBoardgame();
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+
+        //Slettefunktion til Boardgame
+        public void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            BoardgameRepos.DeleteBoardgame();
+            MessageBox.Show("Record Deleted Successfully", "Deleted", MessageBoxButton.OK);
+            BoardgameRepos.ResetBoardgame();
+        }
+
+        //Updatefunktion til Boardgame
+        public void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            BoardgameRepos.UpdateBoardgame();
+            MessageBox.Show("Record Update Successfully", "Updated", MessageBoxButton.OK);
+            BoardgameRepos.ResetBoardgame();
+        }
+
+        //Søgefunktion til Boardgame
+        public void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BoardgameRepos.SearchBoardgame();
+            DataGrid1.ItemsSource = dt.DefaultView;
+            DataGrid1.Columns[0].Visibility = Visibility.Collapsed;
+        }
+
+        public void DataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BoardgameRepos.SelectionChangedBoardgame();
+        }
+    }
+}
